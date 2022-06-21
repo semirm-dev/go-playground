7f5acca5472ed378de8dcb6763649d0dca5125ec
Semir Mahovkic <semirm.dev@gmail.com>
2022-06-21 11:54:04 +0200

    go-playground

--- main.go
+++ main.go
@@ -32,6 +32,25 @@ func arrr() {
 	logrus.Info(arr)
 	logrus.Info(arr2)
 	logrus.Info(arr3)
+
+	arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	logrus.Info(arr)
+
+	logrus.Info(arr[1:5])
+
+	// copy rest of the array
+	copy(arr[2:], arr[2+1:]) // dst: 3, 4..., start: 4, 5...
+	//arr[len(arr)-1] = nil
+	arr = arr[:len(arr)-1]
+
+	logrus.Info(arr)
+
+	// replace with last element
+	arr[3] = arr[len(arr)-1]
+	//arr[len(arr)-1] = nil
+	arr = arr[:len(arr)-1]
+
+	logrus.Info(arr)
 }
 
 // we can use context to close goroutine - instead of done chan
